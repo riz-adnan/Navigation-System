7064a64aa26246bea63d18535d9001f61d1f07ab
Adnan Abbas Rizvi <117975824+riz-adnan@users.noreply.github.com>
2024-01-29 18:47:48 +0530

    finall

--- tests.py
+++ tests.py
@@ -26,6 +26,37 @@ if current_location:
 else:
     print("Unable to determine current location.")
 
-@app.route('/')
+
 def index():
     return render_template('index.html', placeCode="")
+
+def publish_map():
+    latitude = request.form.get('latitude')
+    longitude = request.form.get('longitude')
+    placeCode = request.form.get('placeCode')
+    link = request.form.get('link')
+    print(latitude,longitude,placeCode,link)
+    map = Map(latitude=latitude,longitude=longitude,placeCode=placeCode,link=link)
+    db.session.add(map)
+    db.session.commit()
+    return render_template('success.html')
+
+
+def handle_form():
+    placeCode = request.form.get('placeCode')
+    print(placeCode)
+    # Save to localStorage or perform any other action
+    current_location = get_current_location()
+    zoom_location = get_coordinates(placeCode)
+    if current_location:
+        currlatitude = current_location[0]
+        currlongitude = current_location[1]
+        latitude = zoom_location.json['latitude']
+        longitude = zoom_location.json['longitude']
+        print("lat: ",latitude,"long: ",longitude)
+        return render_template('map.html',placeCode=placeCode,latitude=latitude,longitude=longitude,currlatitude=13.708096,currlongitude=79.594008)
+    else:
+        print("Unable to determine current location.")
+
+
+    return render_template('index.html', placeCode=placeCode)
